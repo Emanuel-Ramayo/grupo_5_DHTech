1841132a9282193f729fa396b442038a4f194e45
Axel <axelviv@live.com.ar>
2023-11-04 05:00:27 -0300

    Crear la estructura de Archivos utilizando Node.JS y Express

---
+++ app.js
@@ -0,0 +1,31 @@
+
+const express = require('express');
+const app = express();
+const path = require('node:path');
+
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
+
+app.listen(3030, () => console.log('Servidor iniciado en el puerto 3030'));
+
+
+//Links
+app.get('/', function(req, res){
+    res.sendFile(path.resolve(__dirname, './views/index.html'))
+})
+
+app.get('/login', function(req, res){
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
+})
+
+app.get('/register', function(req, res){
+    res.sendFile(path.resolve(__dirname, './views/register.html'))
+})
+
+app.get('/productCart', function(req, res){
+    res.sendFile(path.resolve(__dirname, './views/productCart.html'))
+})
+
+app.get('/productDetail', function(req, res){
+    res.sendFile(path.resolve(__dirname, './views/productDetail.html'))
+})
