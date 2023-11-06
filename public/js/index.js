89ead1ccf1706b110a6a8e83295ff2c1e01c1d52
Agustin Carrizo <jcarrizoagustin@gmail.com>
2023-11-06 01:33:02 -0300

    Agregando seccion de novedades

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,45 @@
 
 const menuBtn = document.getElementById("menuBtn");
-const menu = document.getElementById("menu")
+const menu = document.getElementById("menu");
 
+const btnLeft = document.getElementById("btnLeft");
+const btnRigth = document.getElementById("btnRigth");
+const imgContainer = document.getElementById("imgContainer");
+const IMAGENES = [
+    './images/monitor.png',
+    './images/monitor2.png',
+    './images/monitor3.png'
+]
+
+let index = 0
 menuBtn.addEventListener('click', () => {
     menu.classList.toggle('showMenu')
 })
+
+function changeImageNovedades(index,cb) {
+    cb(index)
+}
+
+btnLeft.addEventListener('click', () => {
+    console.log(`Izquierda. Index es: ${index}`)
+    changeImageNovedades(index,changeLeft)
+})
+
+btnRigth.addEventListener('click', () => {
+    console.log(`Derecha. Index es: ${index}`)
+    changeImageNovedades(index,changeRigth)
+})
+
+function changeLeft(index) {
+    if(index > 0){
+        imgContainer.setAttribute('src',IMAGENES[index-1])
+        index--
+    }
+}
+
+function changeRigth(index) {
+    if(index < IMAGENES.length - 1){
+        imgContainer.setAttribute('src',IMAGENES[index+1])
+        index++
+    }
+}
