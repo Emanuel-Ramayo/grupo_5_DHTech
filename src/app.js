2ad958455b71222b6fcbe7fdaee1c149ddc57642
Dhrygon <ByDhrygon@gmail.com>
2023-12-04 20:07:27 -0300

    Pequeños cambios en partials!

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
-const app = express();
 const path = require('node:path');
+const app = express();
+const PORT = process.env.PORT ?? 3030;
+app.listen(PORT, err =>{
+    console.log(err? err : `Servidor Iniciado en http://localhost:${PORT}`);
+});
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -10,36 +14,13 @@ const publicPath = path.resolve(__dirname, '../public');
 app.use(express.static(publicPath));
 
 
-app.listen(3030, () => console.log('Servidor iniciado en el puerto 3030'));
-
-
-//Links
-/* app.get('/', function(req, res){
-    //Aca hice un Cambio en app.get('/') a app.get('/index'), debido a que no funcionaba al levantar el server
-    res.sendFile(path.resolve(__dirname, './views/index.html'))
-})
-
-app.get('/login', function(req, res){
-    res.sendFile(path.resolve(__dirname, './views/login.html'))
-})
-
-
-
-app.get('/productCart', function(req, res){
-    res.sendFile(path.resolve(__dirname, './views/productCart.html'))
-})
-
-app.get('/productDetail', function(req, res){
-    res.sendFile(path.resolve(__dirname, './views/productDetail.html'))
-}) */
-
-
 /* Llamamos al archivo de ruotes correspondiente */
 const rutaIndex = require('./routers/indexRouter.js');
 const rutaProductDetail = require('./routers/productDetailRouter.js');
 const rutaProductCart = require('./routers/productCartRouter.js');
 const rutaRegister = require('./routers/registerRouter.js');
 const rutaLogin = require('./routers/loginRouter.js');
+const { log } = require('node:console');
 
 
 /* Link con app.use a la variable registerRouter */
